686c89e7b994da612da5692c64cc058b57f60d46
tlilecode <tlilecode@gmail.com>
2019-02-22 11:46:01 -0800

    Add default max height and width sizes to each of the sections within the portfolio.

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -1,6 +1,7 @@
 const navItems = document.querySelector(".nav-items");
 const navBtn = document.querySelector(".nav-collapse");
 
+//Toggles viewing the navigation menu when selecting the hamburger menu icon when the viewport is at a mobile device size
 navBtn.addEventListener("click", function() {
   navItems.classList.toggle("hide");
   navItems.classList.toggle("show");
