a65468ea619903d61790be68b13199633a0c2756
tlilecode <tlilecode@gmail.com>
2019-02-22 16:27:25 -0800

    Add preloader to display until rest of the site has loaded.

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -1,5 +1,11 @@
 const navItems = document.querySelector(".nav-items");
 const navBtn = document.querySelector(".nav-collapse");
+const loader = document.getElementById("loading-container");
+
+//Removes preloader after the rest of the window has loaded
+window.onload = function myFunction() {
+  loader.style.display = "none";
+};
 
 //Toggles viewing the navigation menu when selecting the hamburger menu icon when the viewport is at a mobile device size
 navBtn.addEventListener("click", function() {
